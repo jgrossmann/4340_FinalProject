2250a7bc5db795b8257d210570127e3dbcae711b
jchuang-columbia <tc2580@columbia.edu>
2014-11-27 16:06:20 -0500

    mux 5 to1

--- Design/NOC/crossbar_switch/mux/mux_5to1.sv
+++ Design/NOC/crossbar_switch/mux/mux_5to1.sv
@@ -20,11 +20,11 @@ always_comb begin
    
    case (sel_i)
    
-   3'b000 : data_n_i = data_o_temp; 
-   3'b001 : data_s_i = data_o_temp; 
-   3'b010 : data_w_i = data_o_temp; 
-   3'b011 : data_e_i = data_o_temp; 
-   3'b100 : data_l_i = data_o_temp; 
+   3'b000 : data_o_temp = data_n_i ; 
+   3'b001 : data_o_temp = data_s_i ; 
+   3'b010 : data_o_temp = data_w_i ; 
+   3'b011 : data_o_temp = data_e_i ;
+   3'b100 : data_o_temp = data_l_i ;
    
    default : begin 
                    data_o_temp = 16'bxxxx_xxxx_xxxx_xxxx;            
