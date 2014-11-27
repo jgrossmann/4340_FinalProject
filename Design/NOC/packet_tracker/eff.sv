a8cd3ca73c601ebd4c7b869d232401a2c484551c
jchuang-columbia <tc2580@columbia.edu>
2014-11-27 17:54:27 -0500

    packet tracker added

---
+++ Design/NOC/packet_tracker/eff.sv
@@ -0,0 +1,13 @@
+// flip flop
+
+module eff #(parameter DATA_WIDTH = 32) (
+	input clk,
+	input [DATA_WIDTH-1 : 0] data_i,
+	output logic [DATA_WIDTH-1 : 0] data_o
+);
+
+	always_ff @(posedge clk) begin
+			data_o <= data_i;
+	end
+
+endmodule
