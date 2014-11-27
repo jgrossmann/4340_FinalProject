66e4e6654d0e496f6bf0fa3c52da46bb6cac6bbb
jchuang-columbia <tc2580@columbia.edu>
2014-11-27 18:25:43 -0500

    yx processor debug

--- Design/NOC/yx_processor/yx_processor.sv
+++ Design/NOC/yx_processor/yx_processor.sv
@@ -12,19 +12,19 @@ logic [2:0] yx_addr_o_temp;
 
 always_comb begin 
 
-if yx_addr_header_i [3:0] > yx_addr_router_i [3:0] begin 
+if (yx_addr_header_i [3:0] > yx_addr_router_i [3:0]) begin 
 
 yx_addr_o_temp = 3'b001; 
 
-end else if yx_addr_header_i [3:0] < yx_addr_router_i [3:0]
+end else if (yx_addr_header_i [3:0] < yx_addr_router_i [3:0]) begin
 
 yx_addr_o_temp = 3'b000;
 
-end else if yx_addr_header_i [7:4] > yx_addr_router_i [7:4]
+end else if (yx_addr_header_i [7:4] > yx_addr_router_i [7:4]) begin
 
 yx_addr_o_temp = 3'b011;
 
-end else if yx_addr_header_i [7:4] < yx_addr_router_i [7:4]
+end else if (yx_addr_header_i [7:4] < yx_addr_router_i [7:4]) begin
 
 yx_addr_o_temp = 3'b010;
 
