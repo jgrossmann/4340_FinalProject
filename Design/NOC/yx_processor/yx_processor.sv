40b3a2f9a3ecb53c8458c3212c70db4a2b8dbc51
jchuang-columbia <tc2580@columbia.edu>
2014-11-27 18:31:20 -0500

    yx processor debug

--- Design/NOC/yx_processor/yx_processor.sv
+++ Design/NOC/yx_processor/yx_processor.sv
@@ -7,24 +7,27 @@ output yx_addr_o
 
 );
 	
-logic [2:0] yx_addr_o_temp; 
+logic [2:0] yx_addr_o_temp;
+logic [7:0] yx_addr_header_i_temp = yx_addr_header_i;
+logic [7:0] yx_addr_router_i_temp = yx_addr_router_i
+
 
 
 always_comb begin 
 
-if (yx_addr_header_i [3:0] > yx_addr_router_i [3:0]) begin 
+if (yx_addr_header_i_temp [3:0] > yx_addr_router_i_temp [3:0]) begin 
 
 yx_addr_o_temp = 3'b001; 
 
-end else if (yx_addr_header_i [3:0] < yx_addr_router_i [3:0]) begin
+end else if (yx_addr_header_i_temp [3:0] < yx_addr_router_i_temp [3:0]) begin
 
 yx_addr_o_temp = 3'b000;
 
-end else if (yx_addr_header_i [7:4] > yx_addr_router_i [7:4]) begin
+end else if (yx_addr_header_i_temp [7:4] > yx_addr_router_i_temp [7:4]) begin
 
 yx_addr_o_temp = 3'b011;
 
-end else if (yx_addr_header_i [7:4] < yx_addr_router_i [7:4]) begin
+end else if (yx_addr_header_i_temp [7:4] < yx_addr_router_i_temp [7:4]) begin
 
 yx_addr_o_temp = 3'b010;
 
