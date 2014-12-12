d91c86c1b9c7a10d951127ca4ce1b9f43411f4d0
jchuang-columbia <tc2580@columbia.edu>
2014-12-12 13:33:17 -0500

    arbiter

--- Design/NOC/arbiter/rr_overall/n_rr_processor.sv
+++ Design/NOC/arbiter/rr_overall/n_rr_processor.sv
@@ -88,29 +88,39 @@ rr_register_1000 rrr_1000(
 
 );
 
- logic rr_priority_1000_i [3] = (s_to_n_desire == rr_register_1000_order[3]);  
- logic rr_priority_1000_i [2] = (w_to_n_desire == rr_register_1000_order[2]);  
- logic rr_priority_1000_i [1]= (e_to_n_desire == rr_register_1000_order[1]);  
- logic rr_priority_1000_i [0]= (l_to_n_desire == rr_register_1000_order[0]);  
- logic rr_priority_1000_all = rr_priority_1000_i [3] + rr_priority_1000_i [2] + rr_priority_1000_i [1] + rr_priority_1000_i [0]; 
+ logic [3:0] rr_priority_1000_i; 
+ logic [3:0] rr_priority_0100_i; 
+ logic [3:0] rr_priority_0010_i;
+ logic [3:0] rr_priority_0001_i; 
+ logic rr_priority_1000_all; 
+ logic rr_priority_0100_all; 
+ logic rr_priority_0010_all; 
+ logic rr_priority_0001_all; 
+
  
- logic rr_priority_0100_i [3] = (s_to_n_desire == rr_register_0100_order[3]);  
- logic rr_priority_0100_i [2] = (w_to_n_desire == rr_register_0100_order[2]);  
- logic rr_priority_0100_i [1]= (e_to_n_desire == rr_register_0100_order[1]);  
- logic rr_priority_0100_i [0]= (l_to_n_desire == rr_register_0100_order[0]);  
- logic rr_priority_0100_all = rr_priority_0100_i [3] + rr_priority_0100_i [2] + rr_priority_0100_i [1] + rr_priority_0100_i [0];
+ rr_priority_1000_i [3] = (s_to_n_desire == rr_register_1000_order[3]);  
+ rr_priority_1000_i [2] = (w_to_n_desire == rr_register_1000_order[2]);  
+ rr_priority_1000_i [1]= (e_to_n_desire == rr_register_1000_order[1]);  
+ rr_priority_1000_i [0]= (l_to_n_desire == rr_register_1000_order[0]);  
+ rr_priority_1000_all = rr_priority_1000_i [3] + rr_priority_1000_i [2] + rr_priority_1000_i [1] + rr_priority_1000_i [0]; 
  
- logic rr_priority_0010_i [3] = (s_to_n_desire == rr_register_0010_order[3]);  
- logic rr_priority_0010_i [2] = (w_to_n_desire == rr_register_0010_order[2]);  
- logic rr_priority_0010_i [1]= (e_to_n_desire == rr_register_0010_order[1]);  
- logic rr_priority_0010_i [0]= (l_to_n_desire == rr_register_0010_order[0]);  
- logic rr_priority_0010_all = rr_priority_0010_i [3] + rr_priority_0010_i [2] + rr_priority_0010_i [1] + rr_priority_0010_i [0];
+ rr_priority_0100_i [3] = (s_to_n_desire == rr_register_0100_order[3]);  
+ rr_priority_0100_i [2] = (w_to_n_desire == rr_register_0100_order[2]);  
+ rr_priority_0100_i [1]= (e_to_n_desire == rr_register_0100_order[1]);  
+ rr_priority_0100_i [0]= (l_to_n_desire == rr_register_0100_order[0]);  
+ rr_priority_0100_all = rr_priority_0100_i [3] + rr_priority_0100_i [2] + rr_priority_0100_i [1] + rr_priority_0100_i [0];
  
- logic rr_priority_0001_i [3] = (s_to_n_desire == rr_register_0001_order[3]);  
- logic rr_priority_0001_i [2] = (w_to_n_desire == rr_register_0001_order[2]);  
- logic rr_priority_0001_i [1]= (e_to_n_desire == rr_register_0001_order[1]);  
- logic rr_priority_0001_i [0]= (l_to_n_desire == rr_register_0001_order[0]);  
- logic rr_priority_0001_all = rr_priority_0001_i [3] + rr_priority_0001_i [2] + rr_priority_0001_i [1] + rr_priority_0001_i [0];
+ rr_priority_0010_i [3] = (s_to_n_desire == rr_register_0010_order[3]);  
+ rr_priority_0010_i [2] = (w_to_n_desire == rr_register_0010_order[2]);  
+ rr_priority_0010_i [1]= (e_to_n_desire == rr_register_0010_order[1]);  
+ rr_priority_0010_i [0]= (l_to_n_desire == rr_register_0010_order[0]);  
+ rr_priority_0010_all = rr_priority_0010_i [3] + rr_priority_0010_i [2] + rr_priority_0010_i [1] + rr_priority_0010_i [0];
+ 
+ rr_priority_0001_i [3] = (s_to_n_desire == rr_register_0001_order[3]);  
+ rr_priority_0001_i [2] = (w_to_n_desire == rr_register_0001_order[2]);  
+ rr_priority_0001_i [1]= (e_to_n_desire == rr_register_0001_order[1]);  
+ rr_priority_0001_i [0]= (l_to_n_desire == rr_register_0001_order[0]);  
+ rr_priority_0001_all = rr_priority_0001_i [3] + rr_priority_0001_i [2] + rr_priority_0001_i [1] + rr_priority_0001_i [0];
  
  logic [2:0] priority_mux_select; 
  
