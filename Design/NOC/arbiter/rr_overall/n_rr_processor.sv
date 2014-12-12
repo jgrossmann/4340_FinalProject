740eb694c8962f0730ff830f9b1b938f28e81ae2
jchuang-columbia <tc2580@columbia.edu>
2014-12-12 13:48:27 -0500

    arbiter

--- Design/NOC/arbiter/rr_overall/n_rr_processor.sv
+++ Design/NOC/arbiter/rr_overall/n_rr_processor.sv
@@ -88,7 +88,7 @@ rr_register_1000 rrr_1000(
 
 );
 
- logic [3:0] rr_priority_1000_i; 
+// logic [3:0] rr_priority_1000_i; 
  logic [3:0] rr_priority_0100_i; 
  logic [3:0] rr_priority_0010_i;
  logic [3:0] rr_priority_0001_i; 
@@ -97,7 +97,7 @@ rr_register_1000 rrr_1000(
  logic rr_priority_0010_all; 
  logic rr_priority_0001_all;
  
- rr_priority_1000_i[3] = (s_to_n_desire == rr_register_1000_order[3]);  
+ logic rr_priority_1000_i[3] = (s_to_n_desire == rr_register_1000_order[3]);  
  rr_priority_1000_i[2] = (w_to_n_desire == rr_register_1000_order[2]);  
  rr_priority_1000_i[1] = (e_to_n_desire == rr_register_1000_order[1]);  
  rr_priority_1000_i[0] = (l_to_n_desire == rr_register_1000_order[0]);  
