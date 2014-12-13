c36da80ccd18ed816670ca947632a0fb1e8617a0
jchuang-columbia <tc2580@columbia.edu>
2014-12-12 23:24:32 -0500

    input buffer

--- Design/tb/input_buffer/modules/input_buffer_test.sv
+++ Design/tb/input_buffer/modules/input_buffer_test.sv
@@ -52,7 +52,7 @@ logic buf_empty_o_temp;
 
 always_comb begin 
 
-buf_empty_o_temp = (ram_raddr_temp === ram_waddr_temp);  
+buf_empty_o_temp = (ram_raddr_temp [3] && ram_waddr_temp [3]);  
 
 end 
 
