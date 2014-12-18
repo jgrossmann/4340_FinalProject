dac472bb78f67d4b6ff605819a2ac562200766c8
jchuang-columbia <tc2580@columbia.edu>
2014-12-18 14:18:40 -0500

    arbiter debug

--- Design/NOC/arbiter/nexthop_register/e_nexthop_register.sv
+++ Design/NOC/arbiter/nexthop_register/e_nexthop_register.sv
@@ -14,18 +14,26 @@ output [2:0] nhr_address_o
 	
 logic [2:0] data_o_temp; 
 
+logic [2:0] nhr_address_i_temp; 
+
 enable_eff_nr #(.DATA_WIDTH(3)) ff (
 	
 	.clk(clk),
 	.reset(reset),
 	.write_enable_i(nhr_write_i),
 	.data_o(data_o_temp),
-	.data_i(nhr_address_i)
+	.data_i(nhr_address_i_temp)
 	);
 
 always_comb begin
 	if(ib_empty_i | reset) begin
-		nhr_address_i = 3'b011; 
+		nhr_address_i_temp = 3'b011; 
+	end
+	
+	else begin
+	
+	nhr_address_i_temp = nhr_address_i; 
+	
 	end
 end
 
--- Design/NOC/arbiter/nexthop_register/l_nexthop_register.sv
+++ Design/NOC/arbiter/nexthop_register/l_nexthop_register.sv
@@ -13,6 +13,7 @@ output [2:0] nhr_address_o
 );
 	
 logic [2:0] data_o_temp; 
+logic [2:0] nhr_address_i_temp; 
 
 enable_eff_nr #(.DATA_WIDTH(3)) ff (
 	
@@ -20,13 +21,19 @@ enable_eff_nr #(.DATA_WIDTH(3)) ff (
 	.reset(reset),
 	.write_enable_i(nhr_write_i),
 	.data_o(data_o_temp),
-	.data_i(nhr_address_i)
+	.data_i(nhr_address_i_temp)
 	);
 
 always_comb begin
 	if(ib_empty_i | reset) begin
-		nhr_address_i = 3'b100; 
+		nhr_address_i_temp = 3'b100; 
 	end
+	
+	else begin
+	
+	nhr_address_i_temp = nhr_address_i; 
+	
+	end 
 end
 
 assign nhr_address_o = data_o_temp;  
