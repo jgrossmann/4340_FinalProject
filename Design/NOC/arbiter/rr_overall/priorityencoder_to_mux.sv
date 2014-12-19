ccb4103069693773ca0f850922964d00943599c0
jchuang-columbia <tc2580@columbia.edu>
2014-12-19 00:33:14 -0500

    arbiter

--- Design/NOC/arbiter/rr_overall/priorityencoder_to_mux.sv
+++ Design/NOC/arbiter/rr_overall/priorityencoder_to_mux.sv
@@ -11,8 +11,11 @@ module priorityencoder_to_mux(
 );
 
 logic [4:0] rr_priority_i_temp; 
- 
- logic [2:0] rr_priority_to_cs_o_temp; 
+
+logic [2:0] rr_priority_to_cs_o_temp; 
+
+logic [2:0] data_i_temp; 
+logic [2:0] data_o_temp; 
  
  eff_p_to_mux #(.DATA_WIDTH(3)) ff (
 	
@@ -21,7 +24,7 @@ logic [4:0] rr_priority_i_temp;
  	.data_i(data_i_temp)
  	);
 
-	always_comb begin
+ always_comb begin
 
  rr_priority_i_temp [4] = rr_priority_n_i; 
  rr_priority_i_temp [3] = rr_priority_s_i; 
@@ -30,33 +33,35 @@ logic [4:0] rr_priority_i_temp;
  rr_priority_i_temp [0] = rr_priority_l_i;  
  
  if(rr_change_order_i | (rr_priority_i_temp == 5'b00000)) begin
- 	data_i = 3'b111;
+ 	data_i_temp = 3'b111;
  end
- 		rr_priority_i_temp = rr_priority_i_temp & data_o;
+ 
+ 
+ 		rr_priority_i_temp = rr_priority_i_temp & data_o_temp;
 		casex (rr_priority_i_temp)
 			5'b1xxxx:  begin 
 				rr_priority_to_cs_o_temp = 3'b000; 
-				data_i = 3'b000;
+				data_i_temp = 3'b000;
 			end 
 
 			5'b01xxx:  begin 
 				rr_priority_to_cs_o_temp = 3'b001;
-				data_i = 3'b001; 
+				data_i_temp = 3'b001; 
 			end
 
             5'b001xx:  begin 
 				rr_priority_to_cs_o_temp = 3'b010;
-				data_i = 3'b010; 
+				data_i_temp = 3'b010; 
 			end
 
  			5'b0001x:  begin 
 				rr_priority_to_cs_o_temp = 3'b011;
-				data_i = 3'b010; 
+				data_i_temp = 3'b010; 
 			end
 
 			5'b00001:  begin 
 				rr_priority_to_cs_o_temp = 3'b100;
-				data_i = 3'b010;
+				data_i_temp = 3'b010;
 			end
 			
 			5'b00000:  begin 
@@ -64,6 +69,8 @@ logic [4:0] rr_priority_i_temp;
 			end
 
 		endcase
+
+end 
 
 assign rr_priority_to_cs_o = rr_priority_to_cs_o_temp; 
 
